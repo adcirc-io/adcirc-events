eee11c9f9352508f2c95e8c8a2eef69b1cbafe71
Tristan <atdyer@ncsu.edu>
2017-03-09 12:04:12 -0500

    Add once

---
+++ src/events.js
@@ -0,0 +1,142 @@
+
+function dispatcher ( object ) {
+
+    object = object || Object.create( null );
+
+    var _listeners = {};
+    var _oneoffs = {};
+
+    object.on = function ( type, listener ) {
+
+        if ( !arguments.length ) return object;
+        if ( arguments.length == 1 ) return _listeners[ type ];
+
+        if ( _listeners[ type ] === undefined ) {
+
+            _listeners[ type ] = [];
+
+        }
+
+        if ( _listeners[ type ].indexOf( listener ) === - 1 ) {
+
+            _listeners[ type ].push( listener );
+
+        }
+
+        return object;
+
+    };
+
+    object.once = function ( type, listener ) {
+
+        if ( !arguments.length ) return object;
+        if ( arguments.length == 1 ) return _oneoffs[ type ];
+
+        if ( _oneoffs[ type ] === undefined ) {
+
+            _oneoffs[ type ] = [];
+
+        }
+
+        if ( _oneoffs[ type ].indexOf( listener ) === - 1 ) {
+
+            _oneoffs[ type ].push( listener );
+
+        }
+
+        return object;
+
+    };
+
+    object.off = function ( type, listener ) {
+
+        var listenerArray = _listeners[ type ];
+        var oneoffArray = _oneoffs[ type ];
+        var index;
+
+        if ( listenerArray !== undefined ) {
+
+            index = listenerArray.indexOf( listener );
+
+            if ( index !== - 1 ) {
+
+                listenerArray.splice( index, 1 );
+
+            }
+
+        }
+
+        if ( oneoffArray !== undefined ) {
+
+            index = oneoffArray.indexOf( listener );
+
+            if ( index !== -1 ) {
+
+                oneoffArray.splice( index, 1 );
+
+            }
+
+        }
+
+        return object;
+
+    };
+
+    object.dispatch = function ( event ) {
+
+        var listenerArray = _listeners[ event.type ];
+        var oneoffArray = _oneoffs[ event.type ];
+
+        var array = [], i, length;
+
+        if ( listenerArray !== undefined ) {
+
+            event.target = object;
+
+            length = listenerArray.length;
+
+            for ( i = 0; i < length; i ++ ) {
+
+                array[ i ] = listenerArray[ i ];
+
+            }
+
+            for ( i = 0; i < length; i ++ ) {
+
+                array[ i ].call( object, event );
+
+            }
+
+        }
+
+        if ( oneoffArray !== undefined ) {
+
+            event.target = object;
+
+            length = oneoffArray.length;
+
+            for ( i = 0; i < length; i ++ ) {
+
+                array[ i ] = oneoffArray[ i ];
+
+            }
+
+            for ( i = 0; i < length; i ++ ) {
+
+                array[ i ].call( object, event );
+
+            }
+
+            _oneoffs[ event.type ] = [];
+
+        }
+
+        return object;
+
+    };
+
+    return object;
+
+}
+
+export { dispatcher }
