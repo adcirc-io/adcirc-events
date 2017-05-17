507c6a00ddd9f6eb1aac1c6816fa060619118a4f
Tristan <atdyer@ncsu.edu>
2017-05-17 14:52:11 -0400

    Add jsnext:main to package.json

--- build/adcirc-events.js
+++ build/adcirc-events.js
@@ -1,4 +1,4 @@
-// https://github.com/adcirc-io/adcirc-events Version 1.0.3. Copyright 2017 Tristan Dyer.
+// https://github.com/adcirc-io/adcirc-events Version 1.0.4. Copyright 2017 Tristan Dyer.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
