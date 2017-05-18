c35e4a9fb425a73b4a45cea3d1c01d29ad7c40e4
Tristan <atdyer@ncsu.edu>
2017-05-18 10:38:15 -0400

    More work on packaging

--- build/adcirc-events.js
+++ build/adcirc-events.js
@@ -1,4 +1,4 @@
-// https://github.com/adcirc-io/adcirc-events Version 1.0.5. Copyright 2017 Tristan Dyer.
+// https://github.com/adcirc-io/adcirc-events Version 1.0.6. Copyright 2017 Tristan Dyer.
 
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
